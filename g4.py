eb1ec0c54bdcf8aaaf845f62addfe10f35b1a31b
aulaghub <sandro.vscarneiro@upe.br>
2024-10-02 01:15:07 -0300

    Add files via upload

---
+++ g4.py
@@ -0,0 +1,13 @@
+def conversor_moedas():
+    valor_reais = float(input("Digite o valor em reais: "))
+    taxa_dolar = 5.25  # Exemplo de taxa de câmbio
+    taxa_euro = 6.00   # Exemplo de taxa de câmbio
+
+    #REMOVER OS 0s E COMPLETAR A CONVERSÃO
+    valor_dolar = 0
+    valor_euro = 0
+
+    print(f"Valor em dólares: {valor_dolar:.2f}")
+    print(f"Valor em euros: {valor_euro:.2f}")
+
+conversor_moedas()
