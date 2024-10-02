dcf53b817c49b0b52fb72e6ab3d1508d8f9e4055
aulaghub <sandro.vscarneiro@upe.br>
2024-10-02 01:09:41 -0300

    Add files via upload

---
+++ g2.py
@@ -0,0 +1,10 @@
+import random
+
+def gerador_nomes():
+    nomes = ["Ana", "Bruno", "Carlos", "Diana", "Eduardo"]
+    sobrenomes = ["Silva", "Souza", "Pereira", "Oliveira", "Costa"]
+    
+    #COMPLETAR A ESCOLHA DOS NOMES
+    print(f"Nome gerado: {nome_completo}")
+
+gerador_nomes()
