080950fdc2eab6bfb49ceeefe4247928d09df67f
aulaghub <sandro.vscarneiro@upe.br>
2024-10-02 01:13:03 -0300

    Add files via upload

---
+++ g3.py
@@ -0,0 +1,8 @@
+import random
+
+def simular_dado():
+    lados = int(input("Digite o número de lados do dado: "))
+    lancamento = 0 #REMOVER O 0 E COMPLETAR O LANÇAMENTO
+    print(f"O resultado do lançamento do dado é: {lancamento}")
+
+simular_dado()
