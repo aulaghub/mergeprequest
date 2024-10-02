7f6309961abf04bf1539e9d3faf5aa6a728284ab
aulaghub2 <aulagithub133@gmail.com>
2024-10-02 01:47:36 -0300

    Update g2.py

--- g2.py
+++ g2.py
@@ -4,7 +4,7 @@ def gerador_nomes():
     nomes = ["Ana", "Bruno", "Carlos", "Diana", "Eduardo"]
     sobrenomes = ["Silva", "Souza", "Pereira", "Oliveira", "Costa"]
     
-    #COMPLETAR A ESCOLHA DOS NOMES
+    nome_completo = f"{random.choice(sobrenomes)} {random.choice(nomes)}"
     print(f"Nome gerado: {nome_completo}")
 
 gerador_nomes()
